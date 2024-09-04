0443bf315c3e063156c4e5be188ef6d0ee1c5fe7
Khalan Le <khalankimle@gmail.com>
2024-09-04 09:39:25 -0400

    m

---
+++ frontend/hostMeNU/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to HostMeNU!</h1>
+            <p>This tool will allow you to organize and plan
+out a dinner party. In the application, you will be able to enter in a list of your favorite
+recipes for an entree, cocktail, and a dessert. Here, you can update, or change or delete
+recipes, and generate a menu for guests, while accessing your personal database of
+recipes!</p>
+        </div>
+    );
+}
+
+export default HomePage;
