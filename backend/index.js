8ab9439e92eb36e9698a48dcf94b45accc82057c
Khalan Le <khalankimle@gmail.com>
2024-09-04 10:08:22 -0400

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,44 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-
-// Import routes
 const entreeRoutes = require('./routes/entreeRoutes');
 const cocktailRoutes = require('./routes/cocktailRoutes');
 const dessertRoutes = require('./routes/dessertRoutes');
 const authRoutes = require('./routes/authRoutes');
-
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Middleware to parse JSON request bodies
 app.use(express.json());
 
-// Add a simple root route handler to avoid "Cannot GET /" errors
 app.get('/', (req, res) => {
     res.send('Working!')
 });
 
-// API Routes
+// Routes
 app.use('/api/entrees', entreeRoutes);
 app.use('/api/cocktails', cocktailRoutes);
 app.use('/api/desserts', dessertRoutes);
 app.use('/api/auth', authRoutes);
 
-// Serve static files in production mode
+// redirect to html
 if (process.env.NODE_ENV === 'production') {
-    // Serve the React frontend build folder
     app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-    // If the request does not match any API route, serve the index.html file
+    // if the request does not match api route, serve the index.html file
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
     });
 }
 
-// MongoDB connection
+// mongo connection
 const mongoURI = 'mongodb+srv://khalankimle:5vAMvST8iWrhjxuf@mongopractice.dlgeczi.mongodb.net/<YourDatabaseName>'; // Ensure to include the database name
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-    .then(() => console.log('MongoDB connected successfully'))
-    .catch(err => console.error('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB error:', err));
 
-// Start the server
+// start server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
